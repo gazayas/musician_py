44c3fd324b6cfba2b03715f0a241bbe650180b7d
gazayas <g-zayas@hotmail.com>
2017-02-07 19:29:58 +0900

    noteの実装

--- note.py
+++ note.py
@@ -1,1 +1,33 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import re # これを消してmusician.pyの方だけに置きたい
+
 class Note:
+
+  SHARP = ["♯"]
+  FLAT  = ["♭"]
+
+  def __init__(self, name):
+    self.name = name
+
+  def is_sharp(self):
+    pattern = r"♯"
+    result = re.search(pattern, self.name)
+    if result:
+      return True
+    else:
+      return False
+
+  def is_flat(self):
+    pattern = r"♭"
+    result = re.search(pattern, self.name)
+    if result:
+      return True
+    else:
+      return False
+
+  def getName(self):
+    return self.name
+
+  def setName(self, name):
+    self.name = name
